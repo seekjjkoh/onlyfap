3a8dbb8d92779b100299a0cc96db80ef4c5a6843
Jia Jun Koh <jjkoh95@gmail.com>
2021-12-22 21:11:06 +0800

    Fix indentations

--- nodejs-async-hooks/index.js
+++ nodejs-async-hooks/index.js
@@ -6,36 +6,36 @@ const asyncLocalStorage = new AsyncLocalStorage();
 const app = express();
 
 const generateRandomID = () => {
-	return Math.floor(Math.random() * 1_000_000_000);
+  return Math.floor(Math.random() * 1_000_000_000);
 };
 
 const sleep = (ms) => new Promise((resolve) => {
-	setTimeout(() => {
-		return resolve(true);
-	}, ms);
+  setTimeout(() => {
+    return resolve(true);
+  }, ms);
 });
 
 const logger = {
-	info: (msg) => {
-		const tracingID = asyncLocalStorage.getStore().get("tracingID");
-		console.log({ time: new Date(), tracingID, msg });
-	},
+  info: (msg) => {
+    const tracingID = asyncLocalStorage.getStore().get("tracingID");
+    console.log({ time: new Date(), tracingID, msg });
+  },
 };
 
 const injectAsyncLocalStorage = (_req, _res, next) => {
-	asyncLocalStorage.run(new Map(), () => {
-		asyncLocalStorage.getStore().set("tracingID", generateRandomID());
-		return next();
-	});
+  asyncLocalStorage.run(new Map(), () => {
+    asyncLocalStorage.getStore().set("tracingID", generateRandomID());
+    return next();
+  });
 };
 
 app.get("/", injectAsyncLocalStorage, async (_req, res) => {
-	logger.info("before work");
-	await sleep(5_000); // mock heavy work here
-	logger.info("after work");
-	return res.send("Hello world");
+  logger.info("before work");
+  await sleep(5_000); // mock heavy work here
+  logger.info("after work");
+  return res.send("Hello world");
 });
 
 app.listen(PORT, () => {
-	console.log("Nodejs express server listening at port:", PORT)
+  console.log("Nodejs express server listening at port:", PORT)
 });
